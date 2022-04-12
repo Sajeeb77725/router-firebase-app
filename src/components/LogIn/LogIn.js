ea06845aa74baca76e5be28c12e7ead5950597e4
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2022-04-12 16:42:35 +0600

    router with firebase

---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <div>
+      <h2>This is log in form</h2>
+    </div>
+  );
+};
+
+export default LogIn;
+<h2>This is log in form</h2>;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5v8TDHbiYH0i21vGsPbyF6bafOnP65tE",
+  authDomain: "router-firebase-integrat-4ab10.firebaseapp.com",
+  projectId: "router-firebase-integrat-4ab10",
+  storageBucket: "router-firebase-integrat-4ab10.appspot.com",
+  messagingSenderId: "103171698582",
+  appId: "1:103171698582:web:3ed6ff31bd489102ec37b7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
